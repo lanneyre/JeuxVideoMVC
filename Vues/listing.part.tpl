4f9b86764b847788bcd272136685bb6feafbd72f
LANNEY Remi <r.lanney@ics-nice.com>
2021-02-04 16:22:23 +0100

    jeu.php inutile

--- Vues/listing.part.tpl
+++ Vues/listing.part.tpl
@@ -1,4 +1,4 @@
 
 <fieldset data-target= "Jeux_<!--Jeux_Id-->">
-    <h2><a href="jeu.php?id=<!--Jeux_Id-->"><!--Jeux_Titre--></a> <small><!--Jeux_Prix-->€</small></h2>
+    <h2><a href="index.php?id=<!--Jeux_Id-->"><!--Jeux_Titre--></a> <small><!--Jeux_Prix-->€</small></h2>
 </fieldset>
--- index.php
+++ index.php
@@ -2,9 +2,22 @@
     require("Models/Autoloader.php");
     Autoloader::register();
 
-    $jeux = Jeu::getAllJeux();
-    $vue = new Vues();
+    if(!empty($_GET['id'])){
+        $jeu = new Jeu();
+        $jeu = $jeu->hydrate($_GET['id']);
+        if(empty($jeu)){
+            header("Location: index.php?erreur=LejeuNexistePas");
+            exit;
+        }
+        $vue = new Vues();
 
-    echo $vue->generateView($jeux);
+        echo $vue->generateView($jeu, "jeu");
+    } else {
+        $jeux = Jeu::getAllJeux();
+        $vue = new Vues();
+
+        echo $vue->generateView($jeux);
+    }
+    
 
     // var_dump(Database::getPicachu());
